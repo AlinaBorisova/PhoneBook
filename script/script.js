1dd852da8503548a4beb9cd978b4c0decc5dee44
Alina Borisova <b-alina1999.borisova@yandex.ru>
2022-07-13 22:13:34 +0300

    Удаление контакта

--- script/script.js
+++ script/script.js
@@ -24,6 +24,10 @@ const data = [
 ];
 
 {
+  const addContactData = contact => {
+    data.push(contact);
+    console.log('data', data);
+  }
   const createContainer = () => {
     const container = document.createElement('div');
     container.classList.add('container');
@@ -170,11 +174,11 @@ const data = [
       },
     ]);
     const table = createTable();
-    const form = createForm();
+    const {form, overlay} = createForm();
     const footer = createFooter(title);
 
     header.headerContainer.append(logo);
-    main.mainContainer.append(buttonGroup.btnWtapper, table, form.overlay);
+    main.mainContainer.append(buttonGroup.btnWtapper, table, overlay);
     app.append(header, main, footer);
 
     return {
@@ -182,8 +186,8 @@ const data = [
       logo,
       btnAdd: buttonGroup.btns[0],
       btnDel: buttonGroup.btns[1],
-      formOverlay: form.overlay,
-      form: form.form,
+      formOverlay: overlay,
+      form,
     }
   };
 
@@ -255,53 +259,35 @@ const data = [
     })
   };
 
-  const init = (selectorApp, title) => {
-    const app = document.querySelector(selectorApp);
-    const phoneBook = renderPhoneBook(app, title);
+  const modalControl = (btnAdd, formOverlay) => {
+    const openModal = () => {
+      formOverlay.classList.add('is-visible');
+    };
 
-    const {
-      list, 
-      logo, 
-      btnAdd, 
-      formOverlay, 
-      btnDel,
-    } = phoneBook;
-
-    //Здесь будет Функционал для следующих уроков
-    const allRow = renderContacts(list, data);
+    const closeModal = () => {
+      formOverlay.classList.remove('is-visible');
+    };
     
-    hoverRow(allRow, logo);
-
-    btnAdd.addEventListener('click', () => {
-      formOverlay.classList.add('is-visible')
-    });
+    btnAdd.addEventListener('click', openModal);
 
     formOverlay.addEventListener('click', e => {
       const target = e.target;
       if (target === formOverlay ||
           target.closest('.close')) {
-        formOverlay.classList.remove('is-visible');
+        closeModal();
       };
     });
-
+    
     const btnClose = document.querySelector('.close');
-    btnClose.addEventListener('click', () => {
-      formOverlay.classList.remove('is-visible');
-    });
+      btnClose.addEventListener('click', closeModal);
 
+    return {
+      closeModal,
+    };
+    
+  };
 
-    document.addEventListener('touchstart', e => {
-      console.log(e.type);
-    });
-
-    document.addEventListener('touchmove', e => {
-      console.log(e.type);
-    });
-
-    document.addEventListener('touchend', e => {
-      console.log(e.type);
-    });
-
+  const deleteControl = (btnDel, list) => {
     btnDel.addEventListener('click', () => {
       document.querySelectorAll('.delete').forEach(del => {
         del.classList.toggle('is-visible');
@@ -314,7 +300,45 @@ const data = [
         target.closest('.contact').remove();
       };
     });
-  
+  };
+
+  const addContactPage = (contact, list) => {
+    list.append(createRow(contact));
+  }
+
+  const formControl = (form, list, closeModal) => {
+    form.addEventListener('submit', e => {
+      e.preventDefault();
+      const formData = new FormData(e.target);
+
+      const newContact = Object.fromEntries(formData);
+      addContactPage(newContact, list);
+      addContactData(newContact);
+
+      form.reset();
+      closeModal();
+    })
+  };
+
+  const init = (selectorApp, title) => {
+    const app = document.querySelector(selectorApp);
+
+    const {
+      list, 
+      logo, 
+      btnAdd, 
+      formOverlay,
+      form, 
+      btnDel,
+    } = renderPhoneBook(app, title);
+
+    //Здесь будет Функционал для следующих уроков
+    const allRow = renderContacts(list, data);
+    const {closeModal} = modalControl(btnAdd, formOverlay);
+    
+    hoverRow(allRow, logo);
+    deleteControl(btnDel, list);
+    formControl(form, list, closeModal);
   };
 
   window.phoneBookInit = init;
